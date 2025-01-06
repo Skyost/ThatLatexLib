b88680fa6f3a66b00b178faec9cbe17b862bcf90
Hugo Delaunay <Skyost@users.noreply.github.com>
2025-01-06 22:48:03 +0100

    Added a `getMacros` method and a `filterUnknownSymbols` to `KatexRenderer`.

--- src/transformer/renderer.ts
+++ src/transformer/renderer.ts
@@ -22,21 +22,40 @@ export class KatexRenderer extends MathRenderer {
    * The function that allows to render math elements.
    *
    * @param {string} element The math element.
-   * @param {[key: string]: string} macros The macros.
    * @returns {string} The rendered math content.
    */
-  override renderMathElement(
-    element: HTMLElement,
-    macros?: { [key: string]: string }
-  ): string {
-    const math = element.text.trim()
-    return katex.renderToString(math, {
-      displayMode: element.getAttribute('env') === 'displaymath', // Determine if it's a display math environment.
-      output: 'html',
-      trust: true,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      strict: (errorCode: any) => errorCode === 'htmlExtension' ? 'ignore' : 'warn',
-      macros
-    })
+  override renderMathElement(element: HTMLElement): string {
+    const math = this.filterUnknownSymbols(element.text.trim())
+    return katex.renderToString(
+      math,
+      {
+        displayMode: element.getAttribute('env') === 'displaymath', // Determine if it's a display math environment.
+        output: 'html',
+        trust: true,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        strict: (errorCode: any) => errorCode === 'htmlExtension' ? 'ignore' : 'warn',
+        macros: this.getMacros()
+      }
+    )
+  }
+
+  /**
+   * Allows to remove unknown symbols.
+   *
+   * @param {string} math The math content.
+   * @return {string} The filtered math content.
+   */
+  filterUnknownSymbols(math: string): string {
+    return math
+  }
+
+  /**
+   * The macros to pass to the KaTeX parser.
+   *
+   * @return The macros.
+   */
+  // eslint-disable-next-line
+  getMacros(): any {
+    return undefined
   }
 }
